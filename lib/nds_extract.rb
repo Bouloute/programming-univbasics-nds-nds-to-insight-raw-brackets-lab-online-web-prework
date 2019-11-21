379a934a858db52c2b740df25a8b4539f92f66c3
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-11-21 16:47:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,9 +8,13 @@ def directors_totals(nds)
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   
-  pp nds
+  #pp nds
+   
+  nds.each { |key, value| pp key }
+  
   
   result = {
+    
   }
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
