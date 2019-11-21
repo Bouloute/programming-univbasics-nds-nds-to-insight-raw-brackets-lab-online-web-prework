9b1930dabc9bce3ecc9b5797b4971d5199370d30
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-11-21 16:49:03 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,9 +8,9 @@ def directors_totals(nds)
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   
-  #pp nds
+  pp nds
    
-  nds.each { |key, value| pp key }
+ # nds.each { |key, value|  key }
   
   
   result = {
