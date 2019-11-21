32b957a1ca3d049745502668dac99f486e7e14a4
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-11-21 17:10:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,7 +13,7 @@ def directors_totals(nds)
   nds.each { |key, value| 
     key.each { |director|
      #hash = { direc
-     pp director  
+     puts director[:name]
      #director.each {|key, value| puts "key: "  + key + "    value :" + value}
       
     }
