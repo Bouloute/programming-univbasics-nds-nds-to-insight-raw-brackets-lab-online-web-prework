5a8db72cb17c0d70eb24cafcee77ca28041e1e73
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-11-21 17:53:44 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,30 +10,16 @@ def directors_totals(nds)
   
   #pp nds
   hash = {}
-  
-  #pp nds[0]
+
   nds.each { |director, value| 
   total = 0
-   puts director[:name]
-  
     director[:movies].each { |movie_info|
       total += movie_info[:worldwide_gross]
-      puts movie_info[:title]
-      puts movie_info[:worldwide_gross]
-      #pp director[:name]
     }
     
     hash[director[:name]]= total
-  #  puts total
-    puts "_______"
-    puts total
-    puts "\n"
   }
-   # pp hash
   
-  result = {
-    
-  }
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
